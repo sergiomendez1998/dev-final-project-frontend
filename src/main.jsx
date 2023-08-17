67b6972ff0ce8c52a1261d3c43481a61ed7d339a
Sergio Santos Mendez <80726481+sergiomendez1998@users.noreply.github.com>
2023-08-17 00:28:21 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles.css'
 import { UsersApp } from './UsersApp'
 
